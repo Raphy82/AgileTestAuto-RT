19570768389f1112b5e9f9c2cc9cd584f7bd2525
Raphy82 <raphy.thomas82@gmail.com>
2022-11-15 17:41:57 +0100

    updated uppgift_1 and test

--- AgileTestAuto-RT/test_uppgift_1.py
+++ AgileTestAuto-RT/test_uppgift_1.py
@@ -1,2 +1,4 @@
-def test_uppgift_1():
-    assert 1 == 1
+from uppgift_1 import Employee
+def test_fullname():
+    emp1 = Employee("James", "White", 11223344, 40000)
+    assert emp1.fullname == "James White"
--- AgileTestAuto-RT/uppgift_1.py
+++ AgileTestAuto-RT/uppgift_1.py
@@ -1,1 +1,13 @@
-print("Hello World")
+
+
+class Employee:
+
+    def __init__(self, first, last, tele, pay):
+        self.first = first
+        self.last = last
+        self.tele = tele
+        self.pay = pay
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
