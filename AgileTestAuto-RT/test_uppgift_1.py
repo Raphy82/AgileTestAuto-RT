07a88c455f3bc2335703db69a25b838940fdcca8
Raphy82 <raphy.thomas82@gmail.com>
2022-11-15 17:56:28 +0100

    updated uppgift_1 and test

--- AgileTestAuto-RT/test_uppgift_1.py
+++ AgileTestAuto-RT/test_uppgift_1.py
@@ -1,4 +1,6 @@
 from uppgift_1 import Employee
 def test_fullname():
-    emp1 = Employee("James", "White", 11223344, 40000)
-    assert emp1.fullname == "James White"
+    emp1 = Employee("Walter", "White", 11223344, 40000)
+    emp2 = Employee("Jesse", "Pinkman", 55667788, 50000)
+    assert emp1.fullname == "Walter White"
+    assert emp2.fullname == "Jesse Pinkman"
