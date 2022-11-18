f6d5ed6dd7da07fc6e45b77b045a74b7f7affde4
Raphy82 <raphy.thomas82@gmail.com>
2022-11-18 09:51:09 +0100

    Refactoring, DRY implemented

--- AgileTestAuto-RT/test_uppgift_1.py
+++ AgileTestAuto-RT/test_uppgift_1.py
@@ -1,21 +1,41 @@
 import pytest
 from uppgift_1 import Employee
 
-def test_fullname():
+
+@pytest.fixture
+def emp1():
     emp1 = Employee("Walter", "White", 11223344, 40000)
+    return emp1
+
+
+@pytest.fixture
+def emp2():
     emp2 = Employee("Jesse", "Pinkman", 55667788, 50000)
+    return emp2
+
+
+#Test fullname works
+def test_fullname(emp1, emp2):
     assert emp1.fullname == "Walter White"
     assert emp2.fullname == "Jesse Pinkman"
+    print(emp1.fullname)
 
-@pytest.mark.xfail() #Create a failed test case that checks if name is no match.
-def test_fullname_invalid():
+#Test Changing name works
+    emp1.first = 'John'
+    assert emp1.fullname == "John White"
+    print(emp1.fullname)
+
+
+#Test wrong name is no match, test will fail
+@pytest.mark.xfail()
+def test_fullname_invalid(emp2):
     with pytest.raises(ValueError):
-        emp2 = Employee("Jesse", "Pinkman", 55667788, 50000)
         assert emp2.fullname == "Sara Pinkman"
 
-def test_email():
-    emp1 = Employee("Walter", "White", 11223344, 40000)
-    emp2 = Employee("Jesse", "Pinkman", 55667788, 50000)
+
+#Test email works
+def test_email(emp1, emp2):
     assert emp1.email == "Walter.White@iths.se"
     assert emp2.email == "Jesse.Pinkman@iths.se"
     print(emp1.email)
+
