55dbf734c13ad6958311f8e3b9e7e8ae71376ad7
Raphy82 <raphy.thomas82@gmail.com>
2022-11-24 13:16:16 +0100

    Added uppgift_2, open Google website

--- AgileTestAuto-RT/uppgift_2.py
+++ AgileTestAuto-RT/uppgift_2.py
@@ -1,1 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
+path = "/Users/superman/PycharmProjects/bin/chromedriver"
+s = Service(path)
+driver = webdriver.Chrome(service=s)
+'#open google website in chrome'
+driver.get("https://www.google.se")
+
+driver.maximize_window()
+driver.close()
