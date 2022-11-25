ba06c87eec94e7f14e3d6d2eebc236f7df3f1745
Raphy82 <raphy.thomas82@gmail.com>
2022-11-25 12:19:02 +0100

    Increased Uppgift_2 with more funcitons

--- AgileTestAuto-RT/test_uppgift_2.py
+++ AgileTestAuto-RT/test_uppgift_2.py
@@ -1,1 +1,9 @@
+import pytest
 
+'# check first product added to basket'
+'# check right price is added to basket for first product'
+'# add second product added to basket'
+'# check right price is added to basket for first product'
+'# check total price in basket'
+'# check discount calculated'
+'# check we go to checkout with all correct information'
--- AgileTestAuto-RT/uppgift_2.py
+++ AgileTestAuto-RT/uppgift_2.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-path = "/Users/superman/PycharmProjects/bin/chromedriver"
-s = Service(path)
-driver = webdriver.Chrome(service=s)
-'#open google website in chrome'
-driver.get("https://www.google.se")
-
-driver.maximize_window()
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+'#open ellos website in chrome'
+driver.get("https://www.netonnet.se")
+driver.fullscreen_window()
+'#Search for product = 1004014'
+elem = driver.find_element(By.XPATH, "//input[@placeholder='Sök produkter, guider och mer...']")
+elem.send_keys("1004014")
+elem.send_keys(Keys.ENTER)
+time.sleep(4)
+'#Add item 1004014 to checkout'
+elem = driver.find_element(By.ID, "BuyButton_ProductPageStandard_1004014")
+elem.send_keys(Keys.ENTER)
+time.sleep(4)
 driver.close()
